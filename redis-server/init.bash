1c2258c97354426a244cf1e1b6a28b0e38bcb1f5
Nguyễn Đức Long <longnd@rabiloo.com.vn>
2020-05-17 23:19:47 +0700

    add redis server

--- redis-server/init.bash
+++ redis-server/init.bash
@@ -1,18 +1,16 @@
 #!/bin/bash
-CONTAINER_NAME=${1-mysql-server}
-DATA_DIR=~/.data/mysql
-PASSWORD=long97
+CONTAINER_NAME=${1-redis-server}
+DATA_DIR=~/.data/redis
 # Stop container
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 # Create new contanier
 docker run \
     --name=$CONTAINER_NAME \
-    -e MYSQL_ROOT_HOST=% -e \
-    MYSQL_ROOT_PASSWORD=$PASSWORD \
-    -v $DATA_DIR:/var/lib/mysql \
+    -v $DATA_DIR:/data \
     --network=database \
-    -p 3306:3306 \
-    -d mysql/mysql-server
+    -p 6379:6379 \
+    --restart always \
+    -d redis
 
 ./start.bash
--- redis-server/start.bash
+++ redis-server/start.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker start mysql-server
+docker start redis-server
